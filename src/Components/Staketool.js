7de732e3ef11fca0f4ffdfa0440edd83006e7ddb
poqopo <tkdwns63@yonsei.ac.kr>
2022-03-14 23:24:58 +0900

    Staking toPeb

--- src/Components/Staketool.js
+++ src/Components/Staketool.js
@@ -1,25 +1,25 @@
-import { useEffect, useState } from "react";
-import Caver from 'caver-js';
+import { useState } from "react";
 import Input from "./INPUT"
 import Button from "./Button"
 import { useDispatch } from "react-redux";
 
-window.global = window;
-// @ts-ignorez
-window.Buffer = window.Buffer || require('buffer').Buffer;
-
 function StakeButton () {
     let dispatch = useDispatch();
-    const caver = new Caver(window.klaytn)
-    // const stakecontract = new caver.klay.Contract("abi", "")
-    const [amount, setAmount] =useState()
-    const onChange =(event) => setAmount(event.target.value)
 
+    const [amount, setAmount] =useState()
+    const onChange = (event) => {
+      setAmount(Math.round(event.target.value))
+    }
     function onClick() {
       dispatch({
-        type : 'ApproveSCAMP',
-        SCAMPamount : amount
+        type : 'ApproveCAMP',
+        CAMPamount : amount
       })
+      setTimeout(() =>
+      dispatch({
+        type : 'CAMPStake',
+        CAMPamount : amount
+      }), 3000)
     }
 
     return (
--- src/Contract.js
+++ src/Contract.js
@@ -3,13 +3,16 @@ import Bankjs from "./abis/Bank.json"
 import SCAMPjs from "./abis/SCAMP.json"
 import CAMPjs from "./abis/CAMP.json"
 import USDCjs from "./abis/MockUSDC.json"
+import Stakejs from "./abis/CAMPStake.json"
 
 const caver = new Caver(window.klaytn)
 const BankContract = new caver.klay.Contract(Bankjs.abi, "0x470aC5e9E098731F0911003218505151e47a6aDD")
 const SCAMPContract = new caver.klay.Contract(SCAMPjs.abi, "0x3eb98F71f96e43005645Badd3AE678c9828b3708")
 const CAMPContract = new caver.klay.Contract(CAMPjs.abi, "0x1628b45f4e529ad14970FcD0d3Af4f0867cb9eD9")
 const USDCContract = new caver.klay.Contract(USDCjs.abi, "0x0548de7dfD8d8E81AD05E2d0FC0B7b02Bd58e96F")
-const initialstate = {BankContract, SCAMPContract, CAMPContract, USDCContract}
+const StakingContract = new caver.klay.Contract(Stakejs.abi, "0xC0C40B7bD1B9Dfec77FECcF43451f61550c6090a")
+
+const initialstate = {BankContract, SCAMPContract, CAMPContract, USDCContract, StakingContract}
 
 export function reducer (state = initialstate, action) {
   switch(action.type) {
@@ -45,6 +48,14 @@ export function reducer (state = initialstate, action) {
       USDCContract.methods.approve(
         "0x470aC5e9E098731F0911003218505151e47a6aDD",
         caver.utils.toPeb(action.USDCamount*1000, 'kpeb')
+      ).send({
+        from : window.klaytn.selectedAddress,
+        gas: '3000000'
+      })
+      return state;
+    case "CAMPStake" :
+      StakingContract.methods.stake(
+        caver.utils.toPeb(action.CAMPamount*1000, 'mKLAY')
       ).send({
         from : window.klaytn.selectedAddress,
         gas: '3000000'

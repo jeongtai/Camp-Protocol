0c683f891e57acaa487b1d4b22bcdc5f3d0776e5
poqopo <tkdwns63@yonsei.ac.kr>
2022-03-18 15:21:52 +0900

    Fund

--- src/routes/Fund.js
+++ src/routes/Fund.js
@@ -15,11 +15,32 @@ const Fund = () =>{
   function onClick() {
     state.USDCContract.methods.setBalance(
       window.klaytn.selectedAddress,
-      caver.utils.toPeb(amount, "KLAY")
+      caver.utils.toPeb(amount, "Mpeb")
     ).send({
       from : window.klaytn.selectedAddress,
       gas : 3000000
     })
+  }
+
+  function addToken () {
+    const tokenAddress = "0xC990B449a26c05Debb8fB04806397bC5BfF5f4Ed"
+    const tokenSymbol = "MUSDC"
+    const tokenDecimals = 6
+  
+    window.klaytn.sendAsync(
+      {
+        method: 'wallet_watchAsset',
+        params: {
+          type: 'ERC20', // Initially only supports ERC20, but eventually more!
+          options: {
+            address: tokenAddress, // The address that the token is at.
+            symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+            decimals: tokenDecimals, // The number of decimals in the token//
+          }
+        },
+        id: Math.round(Math.random() * 100000)
+      }
+    )
   }
     return (
       <div>
@@ -30,6 +51,10 @@ const Fund = () =>{
           text="Staking amount"
         />
         <Button onClick={onClick} text = "amount mockUSDC you want"/>
+        <div>
+        <Button onClick={addToken} text = "Add wallet!"/>
+        </div>
+        
       </div>
     )
 }

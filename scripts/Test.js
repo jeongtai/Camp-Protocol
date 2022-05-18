63881e8f815d79b5e398f704495f93e0ca3827b4
poqopo <tkdwns63@yonsei.ac.kr>
2022-05-18 16:17:39 +0900

    22

--- scripts/Test.js
+++ scripts/Test.js
@@ -7,18 +7,50 @@ const main = async () => {
     const [owner] = await ethers.getSigners();
 
     /* ============= EKL Contract Deploy =============== */
-    // const EKL = "0x807C4E063eb0aC21E8EeF7623A6ed50A8EDe58cA"
-    const EKL3Moon = "0xd83b9dfa49d6c6d2a69554576e712e45a8a13e49"
-    const EKL33MoonGauge = "0xceaaf9f6c8147b2a7cd8bd4e9fa8955b430eb423"
-    const postEKL = "0x0e23bee35717987e71fa8445e4dd750ad718ba8a"
-    const EKLClaimer = "0xAb7f8facb7db88db80F35c96CD67A9e9d381C7Ee"
-    const GaugeController = "0x18428b7826C2588207e39b742c15642B8D9755B4"
-    const KlaySwap = "0xc6a2ad8cc6e4a7e08fc37cc5954be07d499e7654"
-
     EKLFactory = await ethers.getContractFactory("MEKL");
     // let EKL = await EKLFactory.deploy();
-    const EKL = await EKLFactory.attach("0x807C4E063eb0aC21E8EeF7623A6ed50A8EDe58cA");
+    const EKL = await EKLFactory.attach("0x9F3C49F5e83c43815c8710FdEB54876d6F649B14");
     console.log("EKL address is:", await EKL.address);
+    
+    m3MoonFactory = await ethers.getContractFactory("m3Moon");
+    // let m3Moon = await m3MoonFactory.deploy();
+    const m3Moon = await m3MoonFactory.attach("0xeCDDa9623c57e4539BE76EEEb0F43Ca73a441438");
+    console.log("m3Moon address is:", await m3Moon.address);
+
+    mpostEKLFactory = await ethers.getContractFactory("mpostEKL");
+    // let mpostEKL = await mpostEKLFactory.deploy();
+    const mpostEKL = await mpostEKLFactory.attach("0x0228CC95f101871Cc1Ca94F6C248F2840D176f90");
+    console.log("mpostEKL address is:", await mpostEKL.address);
+
+    kpEKLTokenFactory = await ethers.getContractFactory("kpEKLToken");
+    // let kpEKL = await kpEKLTokenFactory.deploy();
+    const kpEKL = await kpEKLTokenFactory.attach("0x4612f95b014814467BDB069422dAE5A427092520");
+    console.log("kpEKL address is:", await kpEKL.address);
+
+    MockLPFactory = await ethers.getContractFactory("MockLP");
+    // let MockLP = await MockLPFactory.deploy();
+    const MockLP = await MockLPFactory.attach("0xd44725E11821534AB6b9a17D80B75d47a907c0e3");
+    console.log("MockLP address is:", await MockLP.address);
+
+    FakeGaugeFactory = await ethers.getContractFactory("FakeGauge");
+    // let FakeGauge = await FakeGaugeFactory.deploy(m3Moon.address);
+    const FakeGauge = await FakeGaugeFactory.attach("0x06b14E4983565Db987a01fD54f4568814d04631f");
+    console.log("FakeGauge address is:", await FakeGauge.address);
+
+    FakeVoteEscrowFactory = await ethers.getContractFactory("FakeVoteEscrow");
+    // let FakeVoteEscrow = await FakeVoteEscrowFactory.deploy(EKL.address, m3Moon.address);
+    const FakeVoteEscrow = await FakeVoteEscrowFactory.attach("0x7c271c85e31F28935C7F90a25d84C822391D9433");
+    console.log("FakeVoteEscrow address is:", await FakeVoteEscrow.address);
+
+    FakeClaimFactory = await ethers.getContractFactory("FakeClaim");
+    // let FakeClaim = await FakeClaimFactory.deploy(EKL.address, mpostEKL.address);
+    const FakeClaim = await FakeClaimFactory.attach("0x380F3993203C9474Be468B0D1BEf4929d0F064A8");
+    console.log("FakeClaim address is:", await FakeClaim.address);
+
+    // FakeGaugeControllerFactory = await ethers.getContractFactory("FakeVote")
+    // // let FakeVote = await FakeGaugeControllerFactory.deploy();
+    // // const FakeVote = await FakeGaugeControllerFactory.attach("0x380F3993203C9474Be468B0D1BEf4929d0F064A8");
+    // console.log("FakeVote address is:", await FakeVote.address);
 
     KSPFactory = await ethers.getContractFactory("MEKL");
     // let EKL = await EKLFactory.deploy();
@@ -26,12 +58,6 @@ const main = async () => {
     console.log("KSP address is:", await KSP.address);
 
     /* =====================================*/
-    
-
-    kpEKLTokenFactory = await ethers.getContractFactory("kpEKLToken");
-    // let kpEKL = await kpEKLTokenFactory.deploy();
-    const kpEKL = await kpEKLTokenFactory.attach("0x08644836b786B69a5082fD4644a3F2D1534B11A8");
-    console.log("kpEKL address is:", await kpEKL.address);
 
     //Booster에 ekl이랑 m3Moon넣어주기.
 

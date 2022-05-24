583e06af58e3f89cc436f4cba8f7b9b12a533474
scrolldown <scrolldown@naver.com>
2022-05-24 17:21:31 +0900

    merge

--- src/index.js
+++ src/index.js
@@ -119,6 +119,18 @@ const GlobalStyle = createGlobalStyle`
 
 let store = createStore(reducer);
 
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, null,
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location))
+
+
 ReactDOM.render(
     <Provider store={store}>
         <ThemeProvider theme={theme}>
--- src/page/root/NotFound.js
+++ src/page/root/NotFound.js
@@ -1,24 +1,15 @@
-function NotFound(){
+function NotFound() {
     console.log(window.location.href)
     console.log(window.location.pathname)
     var l = window.location;
-    const replaceURL = () => {
-        l.replace(l.protocol +'//' + l.host + l.pathname);
-    }
-    
-    //   l.replace(
-    //     l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
-    //     l.pathname.split('/').slice(0, 1 + pathSegmentsToKeep).join('/') + '/?/' +
-    //     l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~') +
-    //     (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
-    //     l.hash
-    //   );
-    // window.location.href.replace(
-    //     window.location.host,
-    //     `app.${window.location.host}
+    var pathSegmentsToKeep = 0;
 
-    return <p>This page not found<br/>
-        <button onClick={()=>replaceURL()}>replace</button>
-    </p>
+    l.replace(
+        l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
+        l.pathname.split('/').slice(0, 1 + pathSegmentsToKeep).join('/') + '/./' +
+        l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~') +
+        (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+        l.hash
+    );
 }
 export default NotFound;

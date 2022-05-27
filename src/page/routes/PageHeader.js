d7d8f4c05ae98daa834fe9f61727fa65804804bd
jeongtai <zzze37@naver.com>
2022-05-27 17:22:44 +0900

    Add medium link & edit Get EKL in header

--- src/page/routes/PageHeader.js
+++ src/page/routes/PageHeader.js
@@ -96,7 +96,7 @@ function PageHeader() {
             <p>
                 {/* 나중에 tokenB 부분 tokenAddress로 바꾸기 */}
                 <a
-                    href="https://klayswap.com/exchange/swap"
+                    href="https://klayswap.com/exchange/swap?input=0x0000000000000000000000000000000000000000&output=0x807c4e063eb0ac21e8eef7623a6ed50a8ede58ca"
                     target="_blank"
                 >
                     Get EKL
